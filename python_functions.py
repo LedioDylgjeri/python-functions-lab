91c0670c851a6812c2f94669a2c8cdac8d81482c
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-11-01 18:56:31 -0400

    complete exercise 2

--- python_functions.py
+++ python_functions.py
@@ -6,3 +6,10 @@ def sum_to(num):
 
 # print(sum_to(6))
 # print(sum_to(10))
+
+def largest(*args):
+  return max(*args)
+
+
+# print(largest([1, 2, 3, 4, 0]))  # returns 4
+# print(largest([10, 4, 2, 231, 91, 54])) # returns 231
